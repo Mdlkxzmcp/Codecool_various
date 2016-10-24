3ec3eb111d149bea4f7040cb46068ed29cc10d1f
Maximiliaan Strother III <maxbax@op.pl>
2016-10-24 22:57:05 +0200

    Added a sub with calculators

---
+++ Calculator/calcl.py
@@ -0,0 +1,39 @@
+def add(num1, num2):
+    print(num1 + num2)
+
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+
+def divide(num1, num2):
+    print(num1 / num2)
+
+
+def numbers(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            quit()
+
+while True:
+
+    num1 = numbers("Enter a number(or a letter to exit): ")
+    operator = input("Enter an operator: ")
+    num2 = numbers("Enter another number: ")
+
+    if (operator == "+"):
+        add(num1, num2)
+    elif (operator == "-"):
+        subtract(num1, num2)
+    elif (operator == "*"):
+        multiply(num1, num2)
+    elif (operator == "/"):
+        divide(num1, num2)
+    else:
+        print("Invalid operator")
---
+++ Calculator/calcm.py
@@ -0,0 +1,81 @@
+adding_list = ["+", "plus", "add"]
+subtracting_list = ["-", "minus", "subtract"]
+multiplying_list = ["*", "multiply"]
+dividing_list = ["/", "divide"]
+
+
+def main():
+    question_handler()
+    if(decision in adding_list):
+        Math_Functions.adding(num1, num2)
+    elif(decision in subtracting_list):
+        Math_Functions.subtracting(num1, num2)
+    elif(decision in multiplying_list):
+        Math_Functions.multiplying(num1, num2)
+    elif(decision in dividing_list):
+        Math_Functions.dividing(num1, num2)
+    else:
+        print("Unexpected situation, calling mentors...")
+
+
+# this function handles the user input
+def question_handler():
+    global num1, num2, decision
+    # while blocks that check if the input is of valid type
+    while True:
+        try:
+            num1 = int(input("Enter the first number you want to work with: "))
+        # if the input cannot be made into a number this happenes
+        except ValueError:
+            print("Why are you playing around?")
+            continue
+        else:
+            break
+    while True:
+        all_operations = ["+", "plus", "add", "-", "minus", "subtract", "*",
+                          "multiply", "/", "divide"]
+        decision = input("Enter desired operation: ")
+        if(decision == "quit"):
+            quit()
+        elif(decision in all_operations):
+            break
+        else:
+            print('This is not a valid operation here, choose one: ', end="")
+            print(all_operations, end="")
+            print(" or type in \"quit\" to exit")
+    while True:
+        try:
+            num2 = int(input("Enter the second number: "))
+        except ValueError:
+            print("Hey stop that!")
+            continue
+        else:
+            break
+
+
+# making use of classes here just for fun
+class Math_Functions:
+
+    def adding(num1, num2):
+        global result
+        result = num1 + num2
+        print("Result: " + format(result))
+
+    def subtracting(num1, num2):
+        global result
+        result = num1 - num2
+        print("Result: " + format(result))
+
+    def multiplying(num1, num2):
+        global result
+        result = num1 * num2
+        print("Result: " + format(result))
+
+    def dividing(num1, num2):
+        global result
+        result = num1 / num2
+        print("Result: " + format(result))
+
+
+if __name__ == '__main__':
+    main()
