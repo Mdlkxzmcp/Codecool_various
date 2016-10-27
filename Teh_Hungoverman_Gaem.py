50661cd9ec10a9f1f1c6e8c2786a93aa935d8c13
Maximiliaan Strother III <maxbax@op.pl>
2016-10-27 18:20:11 +0200

    v 0.2.6    ?
    There are still some things to do but this update brought quite a lot of new stuff...

--- Teh_Hungoverman_Gaem.py
+++ Teh_Hungoverman_Gaem.py
@@ -6,12 +6,16 @@ global current_capital
 
 
 def file_operator():
+    """opens up the countries_and_capitals.txt file and transforms it into a
+    usable list after which the file is closed"""
     global countries_and_capitals
     with open("countries_and_capitals.txt") as list:
         countries_and_capitals = list.read().splitlines()
 
 
 def current_setter():
+    """takes a pair from the countries_and_capitals list and then separates
+    them into two seperate variables ( + creates the current_capital_upper)"""
     global countries_and_capitals
     global current_capital
     global current_capital_upper
@@ -23,6 +27,7 @@ def current_setter():
 
 
 def high_score_screen():
+    """displays the current high scores!"""
     global high_scores
     with open("high_scores.txt") as list:
         high_scores = list.read().splitlines()
@@ -30,13 +35,19 @@ def high_score_screen():
 
 
 def clear():
+    """sets counter, used_letters and life to starting values"""
     global life
     global used_letters
+    global counter
+    counter = 0
     used_letters = []
     life = 5
 
 
-def show_status(current_capital_upper):
+def show_status():
+    """prints out "_ " or the letter itself from the current_capital_upper if
+    it is in the used_letters list"""
+    global current_capital_upper
     global current_country
     global used_letters
     global life
@@ -53,32 +64,40 @@ def show_status(current_capital_upper):
 
 
 def win_screen():
-    global letters_count
+    """the "screen" that is displayed when the game is won. Measures the time
+    spent and shows the counter plus the time of course. Calles the
+    high_score_handler function after which it calls the try_again screen"""
+    global counter
     global current_capital
     global start_time
     global usable_elapsed_time
     elapsed_time = time.time() - start_time
     usable_elapsed_time = str(elapsed_time)[:4]
-    print("Congratulations! You won using", letters_count,
-          "letters! in", usable_elapsed_time, "sec, nice!")
+    print("Congratulations! You won after", counter,
+          "tries! in", usable_elapsed_time, "sec, nice!")
     high_score_handler()
     try_again()
 
 
 def high_score_handler():
+    """asks the user for his name if he won after which it stores the new high
+    score to the high_scores.txt file"""
     global usable_elapsed_time
     player_name = input("Type in your name to add it to the high scores!: ")
     new_high_score = [player_name, " | ", usable_elapsed_time, " | ",
-                      str(letters_count), " | ", current_capital, "\n"]
+                      str(counter), " | ", current_capital, "\n"]
     print("".join(new_high_score))
     with open("high_scores.txt", "a+") as high_scores:
         high_scores.writelines(new_high_score)
 
 
 def try_again():
+    """asks if the user wants to try/play again and depending on the decision
+    either starts a new game by calling clear, current_setter & main
+    functions or quits the program"""
     global life
     high_score_screen()
-    try_again = input("Do you want to try again? yes/no: ")
+    try_again = input("Do you want to play again? yes/no: ")
     if try_again == "yes" or try_again == "y":
         clear()
         current_setter()
@@ -91,37 +110,46 @@ def try_again():
 
 
 def loose_screen():
-    global letters_count
+    """the screen that is displayed when the user looses. Shows elapsed time
+    and calls the try again function"""
+    global counter
     global start_time
     global elapsed_time
     elapsed_time = time.time() - start_time
     print("You lost after", str(elapsed_time)[
-          :4], "secs, using", letters_count, "letters...")
+          :4], "secs, after", counter, "tries...")
     try_again()
 
 
 def current_life():
+    """shows remaining life"""
     global life
     print("Remaining life: ", life)
 
 
 def letter_input():
     global life
-    global letters_count
+    global counter
     choosen_letter = input("Type in a letter: ").upper()
-    if choosen_letter in used_letters:
-        print("You already used this letter")
-        input_situation()
+    if len(choosen_letter) == 1:
+        counter += 1
+        if choosen_letter in used_letters:
+            print("You already used this letter")
+            input_situation()
+        else:
+            used_letters.append(choosen_letter)
+        if choosen_letter not in current_capital_upper:
+            life -= 1
     else:
-        used_letters.append(choosen_letter)
-        letters_count += 1
-    if choosen_letter not in current_capital_upper:
-        life -= 1
+        print("letters please!")
+        letter_input()
 
 
 def word_input():
     global life
+    global counter
     choosen_word = input("Type in a word: ").upper()
+    counter += 1
     if choosen_word == current_capital_upper:
         win_screen()
     else:
@@ -140,26 +168,30 @@ def input_situation():
 
 
 def checker(used_letters, current_capital_upper):
+    """checker(list, string) checks if values from list are all present in
+    the string and then returns a boolean"""
     return set(current_capital_upper).issubset(used_letters)
 
 
 def main():
     global start_time
     start_time = time.time()
+    # ready... set... go!!!
     global life
     while True:
         if life > 0:
+            # this calles the win screen if the user guessed the word :>
             if checker(used_letters, current_capital_upper) is True:
                 win_screen()
-            show_status(current_capital_upper)
+            show_status()
             input_situation()
         else:
+            # or the loose screen if the user lost :<<<
             loose_screen()
 
+# this first prepares the game and then calles the main that starts it
 if __name__ == '__main__':
-    used_letters = []
-    life = 5
-    letters_count = 0
+    clear()
     file_operator()
     current_setter()
     main()
