07d454a9d95b4876a6d18cd8bf115fff88017320
Maximiliaan Strother III <maxbax@op.pl>
2016-10-28 09:56:44 +0200

    made order by calls and logic, still need to sort the high score and display only the 10 best but oh well ascii art might be added one day v.0.3.1

--- Teh_Hungoverman_Gaem.py
+++ Teh_Hungoverman_Gaem.py
@@ -5,6 +5,16 @@ import datetime
 global current_capital
 
 
+def clear():
+    """sets counter, used_letters and life to starting values"""
+    global life
+    global used_letters
+    global counter
+    counter = 0
+    used_letters = []
+    life = 5
+
+
 def file_operator():
     """opens up the countries_and_capitals.txt file and transforms it into a
     usable list after which the file is closed"""
@@ -26,107 +36,25 @@ def current_setter():
     current_country = choosen_pair.split(" |")[0]
 
 
-def high_score_screen():
-    """displays the current high scores!"""
-    global high_scores
-    with open("high_scores.txt") as list:
-        high_scores = list.read().splitlines()
-        print("\n".join(high_scores))
-
-
-def clear():
-    """sets counter, used_letters and life to starting values"""
-    global life
+def checker(current_capital_upper):
     global used_letters
-    global counter
-    counter = 0
-    used_letters = []
-    life = 5
+    """checker(list, string) checks if values from list are all present in
+    the string and then returns a boolean"""
+    used_letters.append(" ")
+    state = set(current_capital_upper).issubset(used_letters)
+    used_letters.remove(" ")
+    return state
 
 
-def show_status():
-    """prints out "_ " or the letter itself from the current_capital_upper if
-    it is in the used_letters list"""
-    global current_capital_upper
-    global current_country
-    global used_letters
-    global life
-    current_life()
-    if life == 1:
-        print("It's the capital of", current_country, ";3")
-    for letter in current_capital_upper:
-        if letter == " ":
-            print(" - ", end="")
-        elif letter in used_letters:
-            print(letter, end="")
-        else:
-            print("_ ", end="")
-    if used_letters:
-        print("  You already tired: ", used_letters)
-
-
-def win_screen():
-    """the "screen" that is displayed when the game is won. Measures the time
-    spent and shows the counter plus the time of course. Calles the
-    high_score_handler function after which it calls the try_again screen"""
-    global counter
-    global current_capital
-    global start_time
-    global usable_elapsed_time
-    elapsed_time = time.time() - start_time
-    usable_elapsed_time = str(elapsed_time)[:4]
-    print("Congratulations! You won after", counter,
-          "tries! in", usable_elapsed_time, "sec, nice!")
-    high_score_handler()
-    try_again()
-
-
-def high_score_handler():
-    """asks the user for his name if he won after which it stores the new high
-    score to the high_scores.txt file"""
-    global usable_elapsed_time
-    player_name = input("Type in your name to add it to the high scores!: ")
-    new_high_score = [player_name, " | ", usable_elapsed_time, " | ",
-                      str(counter), " | ", current_capital, "\n"]
-    print("".join(new_high_score))
-    with open("high_scores.txt", "a+") as high_scores:
-        high_scores.writelines(new_high_score)
-
-
-def try_again():
-    """asks if the user wants to try/play again and depending on the decision
-    either starts a new game by calling clear, current_setter & main
-    functions or quits the program"""
-    global life
-    high_score_screen()
-    try_again = input("Do you want to play again? yes/no: ")
-    if try_again == "yes" or try_again == "y":
-        clear()
-        current_setter()
-        main()
-    elif try_again == "no" or try_again == "n":
-        quit()
+def input_situation():
+    choice = input("Do you want to type in a (l)etter or a (w)ord?: ")
+    if choice == "letter" or choice == "l":
+        letter_input()
+    elif choice == "word" or choice == "w":
+        word_input()
     else:
-        print("yes or no")
-        try_again()
-
-
-def loose_screen():
-    """the screen that is displayed when the user looses. Shows elapsed time
-    and calls the try again function"""
-    global counter
-    global start_time
-    global elapsed_time
-    elapsed_time = time.time() - start_time
-    print("You lost after", str(elapsed_time)[
-          :4], "secs, after", counter, "tries...")
-    try_again()
-
-
-def current_life():
-    """shows remaining life"""
-    global life
-    print("Remaining life: ", life)
+        print("That's not an option.")
+        input_situation()
 
 
 def letter_input():
@@ -158,31 +86,105 @@ def word_input():
         life -= 2
 
 
-def input_situation():
-    choice = input("Do you want to type in a (l)etter or a (w)ord?: ")
-    if choice == "letter" or choice == "l":
-        letter_input()
-    elif choice == "word" or choice == "w":
-        word_input()
-    else:
-        print("That's not an option.")
-        input_situation()
-
-
-def checker(current_capital_upper):
+def show_status():
+    """prints out "_ " or the letter itself from the current_capital_upper if
+    it is in the used_letters list"""
+    global current_capital_upper
+    global current_country
     global used_letters
-    """checker(list, string) checks if values from list are all present in
-    the string and then returns a boolean"""
-    used_letters.append(" ")
-    state = set(current_capital_upper).issubset(used_letters)
-    used_letters.remove(" ")
-    return state
+    global life
+    current_life()
+    if life == 1:
+        print("It's the capital of", current_country, ";3")
+    for letter in current_capital_upper:
+        if letter == " ":
+            print(" - ", end="")
+        elif letter in used_letters:
+            print(letter, end="")
+        else:
+            print("_ ", end="")
+    if used_letters:
+        print("  You already tired: ", used_letters)
+
+
+def current_life():
+    """shows remaining life"""
+    global life
+    print("Remaining life: ", life)
+
+
+def win_screen():
+    """the "screen" that is displayed when the game is won. Measures the time
+    spent and shows the counter plus the time of course. Calles the
+    high_score_handler function after which it calls the try_again screen"""
+    global counter
+    global current_capital
+    global start_time
+    global usable_elapsed_time
+    elapsed_time = time.time() - start_time
+    usable_elapsed_time = str(elapsed_time)[:4]
+    print("Congratulations! You won after", counter,
+          "tries! in", usable_elapsed_time, "sec, nice!")
+    high_score_handler()
+    try_again()
+
+
+def loose_screen():
+    """the screen that is displayed when the user looses. Shows elapsed time
+    and calls the try again function"""
+    global counter
+    global start_time
+    global elapsed_time
+    elapsed_time = time.time() - start_time
+    print("You lost after", str(elapsed_time)[
+          :4], "secs, after", counter, "tries...")
+    try_again()
+
+
+def try_again():
+    """asks if the user wants to try/play again and depending on the decision
+    either starts a new game by calling clear, current_setter & main
+    functions or quits the program"""
+    global life
+    high_score_screen()
+    try_again = input("Do you want to play again? yes/no: ")
+    if try_again == "yes" or try_again == "y":
+        clear()
+        current_setter()
+        main()
+    elif try_again == "no" or try_again == "n":
+        quit()
+    else:
+        print("yes or no")
+        try_again()
+
+
+def high_score_handler():
+    """asks the user for his name if he won after which it stores the new high
+    score to the high_scores.txt file"""
+    global usable_elapsed_time
+    player_name = input("Type in your name to add it to the high scores!: ")
+    day = datetime.datetime.now()
+    new_high_score = [player_name, " | ", day.strftime(
+        "%Y-%m-%d"), " | ", usable_elapsed_time, " | ", str(counter), " | ",
+        current_capital, " | ", "\n"]
+    print("Your score is: ".join(new_high_score))
+    with open("high_scores.txt", "a+") as high_scores:
+        high_scores.writelines(new_high_score)
+
+
+def high_score_screen():
+    """displays the current high scores!"""
+    global high_scores
+    with open("high_scores.txt") as list:
+        high_scores = list.read().splitlines()
+        print("\n".join(high_scores))
 
 
 def main():
     global start_time
-    start_time = time.time()
     # ready... set... go!!!
+    start_time = time.time()
     global life
     while True:
         if life > 0:
