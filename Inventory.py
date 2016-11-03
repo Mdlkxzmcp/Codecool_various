6772ebc1e5e88f71fa76788dd7c1427e634fce61
Maximiliaan Strother III <maxbax@op.pl>
2016-11-03 20:03:19 +0100

    Inventory v0.2
    Added a new function print_table and fixed the add_to_invetory bug causing the inventory to have too many or not enough items in it

--- Inventory.py
+++ Inventory.py
@@ -13,13 +13,34 @@ def display_inventory(inventory):
 
 def add_to_inventory(inventory, items):
     new_items = (dict((item, items.count(item)) for item in set(items)))
-    try:
-        for key, value in new_items.items():
-            inventory[key] += value
-    except KeyError:
-        inventory.setdefault(key, value)
-    print(inventory)
+    iterations = 0
+    while iterations < len(new_items):
+        try:
+            for key, value in new_items.items():
+                if key in inventory:
+                    inventory[key] += value
+                    iterations += 1
+                else:
+                    inventory.setdefault(key, value)
+                    iterations += 1
+        except KeyError:
+            print("woot woot send help")
+    return inventory
 
+
+def print_table(inventory, order):
+    total_number_of_items = 0
+    count_width = 8
+    longest_val = max(len(key) for key in inventory) + 1
+    formula_for_the_line = (longest_val + count_width + longest_val % 2)
+    print("Inventory:")
+    print("{:>7}{:>{}}".format("count", "item name", longest_val + 1))
+    print("-" * formula_for_the_line)
+    for key, value in inventory.items():
+        print("{:{}d} {:>{}}".format(value, count_width - 1, key, longest_val))
+        total_number_of_items += value
+    print("-" * formula_for_the_line)
+    print("Total number of items: {}".format(total_number_of_items))
 
 add_to_inventory(inv, loot)
-display_inventory(inv)
+print_table(inv, "count,desc")
