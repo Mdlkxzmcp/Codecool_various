fd8e8a776031a3bd3774c1efce87d837f616fd6a
Maximiliaan Strother III <maxbax@op.pl>
2016-11-03 21:52:34 +0100

    Inventory v.0.2.1
    Making the code look better and added inventory sorting

--- Inventory.py
+++ Inventory.py
@@ -4,6 +4,7 @@ loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory(inventory):
     total_number_of_items = 0
+
     print("Inventory:")
     for key, value in inventory.items():
         print("{} {}".format(value, key))
@@ -14,6 +15,7 @@ def display_inventory(inventory):
 def add_to_inventory(inventory, items):
     new_items = (dict((item, items.count(item)) for item in set(items)))
     iterations = 0
+
     while iterations < len(new_items):
         try:
             for key, value in new_items.items():
@@ -25,6 +27,7 @@ def add_to_inventory(inventory, items):
                     iterations += 1
         except KeyError:
             print("woot woot send help")
+
     return inventory
 
 
@@ -32,14 +35,25 @@ def print_table(inventory, order):
     total_number_of_items = 0
     count_width = 8
     longest_val = max(len(key) for key in inventory) + 1
-    formula_for_the_line = (longest_val + count_width + longest_val % 2)
+    formula_for_the_line = "-" * (longest_val + count_width + longest_val % 2)
+    if order == "count,desc":
+        sort = True
+    elif order == "count,asc":
+        sort = False
+    else:
+        print("Not now .-.")
+        sort = False
+
     print("Inventory:")
     print("{:>7}{:>{}}".format("count", "item name", longest_val + 1))
-    print("-" * formula_for_the_line)
-    for key, value in inventory.items():
+    print(formula_for_the_line)
+    # -------------------------
+    for key, value in sorted(inventory.items(), key=lambda val: val[1],
+                             reverse=sort):
         print("{:{}d} {:>{}}".format(value, count_width - 1, key, longest_val))
         total_number_of_items += value
-    print("-" * formula_for_the_line)
+    # -------------------------
+    print(formula_for_the_line)
     print("Total number of items: {}".format(total_number_of_items))
 
 add_to_inventory(inv, loot)
