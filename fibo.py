5a726705dc51054166758787135b9eebbdfc2111
Maximiliaan Strother III <maxbax@op.pl>
2016-11-03 13:53:04 +0100

    Fibo v.1
    Modified the file to let the user choose whether he wants only one number or a list of numbers. Should be bug free

--- fibo.py
+++ fibo.py
@@ -1,17 +1,46 @@
 def fibo(number):
-    "function that prints out the given fibonacci number"
+    """function that prints out the given fibonacci number"""
     if number == 0 or number == 1:
         return 1
     else:
         return fibo(number - 1) + fibo(number - 2)
 
+
+def fibos(iteration):
+    """funcion that prints fibonacci numbers up to a given number"""
+    nr_of_iteration = 1
+    num1 = 0
+    num2 = 1
+    while(iteration > 0):
+        print(format(nr_of_iteration) + ": " + format(num1))
+        num1, num2 = num2, num1 + num2
+        iteration -= 1
+        nr_of_iteration += 1
+
 while True:
     try:
-        number = int(
-            input("Which fibo number do you need man? "))
-        print(fibo(number))
-    except ValueError:
-        print("Woah there calm down, numbers please!")
+        choice = str(input(r"Do you want fibo 'number' or 'numbers' man? "))
+    except TypeError:
+        print(r"Hey 'number' or 'numbers' mannnnn")
         continue
     else:
-        break
+        if choice == "numbers":
+            try:
+                iteration = int(
+                    input("How many fibo numbers do ya need ma man? "))
+            except ValueError:
+                print("Woah there calm down, numbers please!")
+                continue
+            else:
+                fibos(iteration)
+        elif choice == "number":
+            try:
+                number = int(
+                    input("Which fibo number do you need man? "))
+                print(fibo(number))
+            except ValueError:
+                print("Woah woah dude man use numbers")
+                continue
+        else:
+            print(r"Told you, 'number' or 'numbers' :|")
+            break
