b80d1fcb2843fc6add295131988aa74da49611d7
Maximiliaan Strother III <maxbax@op.pl>
2017-01-29 21:10:59 +0100

    cosmetic changes part 1/9008

--- Alpha & Beta/Django/WaifuTracker/main_app/models.py
+++ Alpha & Beta/Django/WaifuTracker/main_app/models.py
@@ -6,7 +6,7 @@ class Waifu(models.Model):
 	name = models.CharField(max_length=120)
 	series = models.CharField(max_length=120)
 	rating = models.CharField(max_length=25)
-	height = models.DecimalField(max_digits=6, decimal_places=2)
+	height = models.DecimalField(max_digits=6, decimal_places=0)
 	waifu_of_the_month = models.BooleanField()
 	img_url = models.CharField(max_length=100)
 
